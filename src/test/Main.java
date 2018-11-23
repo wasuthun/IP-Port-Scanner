cc61c541c6945e10cd07e978664a53a585ffc151
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2018-11-23 10:58:01 +0700

    Finished: Ping method

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,12 +1,11 @@
 package test;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -14,36 +13,65 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(final String... args)
-			throws InterruptedException, ExecutionException, UnknownHostException, IOException {
+	public static void main(final String... args) throws InterruptedException, ExecutionException {
 		final ExecutorService es = Executors.newFixedThreadPool(1000);
 		final int timeout = 100;
 		final List<Future<ScanResult>> futures = new ArrayList<>();
-		List<String> ipRange = getIpList("127.0.0.0", 10);
+		List<String> ipRange = getIpList("127.0.0.0", "127.0.0.1");
 		for (String ipaddr : ipRange) {
-			InetAddress inet = InetAddress.getByName(ipaddr);
-			long finish = 0;
-			long start = new GregorianCalendar().getTimeInMillis();
-			if (inet.isReachable(2000)) {
-				finish = new GregorianCalendar().getTimeInMillis();
-				System.out.println(ipaddr + " :Pinged successfully in " + (finish - start + "ms"));
+			if (isReadable(ipaddr)) {
+				System.out.println("ping: " + ping(ipaddr));
 				for (int port = 1; port <= 65535; port++) {
 					futures.add(portIsOpen(es, ipaddr, port, timeout));
 				}
 			} else {
-				System.out.println(ipaddr + " :Ping failed.");
+				System.out.println(ipaddr + " is not reachable");
 			}
 		}
 		es.awaitTermination(200L, TimeUnit.MILLISECONDS);
+
 		int openPorts = 0;
 		for (final Future<ScanResult> f : futures) {
 			if (f.get().isOpen()) {
 				openPorts++;
 				System.out.println(f.get().getIp() + " with " + f.get().getPort() + " Port.");
 			}
+		}
+	}
+
+	public static boolean isReadable(String host) {
+		try {
+			return InetAddress.getByName(host).isReachable(2000);
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	public static int ping(String host) {
+		Pattern pattern = Pattern.compile("Average = (\\d+)ms");
+		Matcher m = null;
+		try {
+			Runtime r = Runtime.getRuntime();
+			System.out.println("Sending Ping Request to " + host);
+			Process p = r.exec("ping -n 1 " + host);
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				m = pattern.matcher(inputLine);
+				if (m.find()) {
+					return Integer.parseInt(m.group(1));
+				}
+			}
+			in.close();
+			return -1;
+		} catch (Exception e) {
+			return -1;
 		}
 	}
 
@@ -82,10 +110,11 @@ public class Main {
 				.append('.').append(tokens[3]).toString();
 	}
 
-	public static List<String> getIpList(String startIp, int number) {
+	public static List<String> getIpList(String startIp, String endIp) {
 		List<String> result = new ArrayList<>();
 		String currentIp = startIp;
-		for (int i = 0; i < number; i++) {
+		result.add(startIp);
+		while (!currentIp.equals(endIp)) {
 			String nextIp = nextIpAddress(currentIp);
 			result.add(nextIp);
 			currentIp = nextIp;
