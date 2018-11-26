b6f46a53e3ccac51cb8b26964571cf0726d00219
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2018-11-26 17:38:37 +0700

    Merged: SceneBuilder branch to master

--- src/application/ConsoleView.java
+++ src/application/ConsoleView.java
@@ -7,17 +7,18 @@ import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
 import util.NetworkScanner;
 import util.ScanResult;
 
-public class ConsoleView implements Observer{
-	
+public class ConsoleView implements Observer {
+
 	private NetworkScanner network;
 
 	public ConsoleView(NetworkScanner network) {
-		this.network=network;
+		this.network = network;
 	}
+
 	@Override
 	public void update(Observable o, Object arg) {
 		ScanResult s = (ScanResult) arg;
-		System.out.printf("%s with %s Port \n",s.getIp(),s.getPort());
+		System.out.printf("%s with %s Port \n", s.getIp(), s.getPort());
 	}
 
 }
